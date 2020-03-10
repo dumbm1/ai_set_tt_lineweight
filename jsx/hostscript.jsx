b61a230112301c3fa6df667215759f98129a1027
Marat Shagiev <m_js@bk.ru>
2020-03-10 20:30:31 +0300

    add host function _mkTestArray

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -18,6 +18,76 @@ function getMinLineWeight() {
 
   alert(rects);
 
+  function _mkTestArray(tmplPath, rects) {
+    var tmplPathArr = [];
+
+    tmplPathArr.push(tmplPath);
+
+    for (var i = 0; i < rects.length; i++) {
+      var tmplPathEl = tmplPath.duplicate();
+      tmplPathArr.push(tmplPathEl);
+    }
+
+    for (var i = 0; i < rects.length; i++) {
+      executMenuCommand('deselectall');
+      rects[i].selected = true;
+      tmplPahtArr[i].selected = true;
+      __intersectSelection();
+    }
+
+    /**
+     * make action that try to intersect selection paths
+     * */
+    function __intersectSelection() {
+      var actStr = '' +
+                   '/version 3' +
+                   '/name [ 19' +
+                   '	496e746572736563745f73656c656374696f6e' +
+                   ']' +
+                   '/isOpen 0' +
+                   '/actionCount 1' +
+                   '/action-1 {' +
+                   '	/name [ 19' +
+                   '		496e746572736563745f73656c656374696f6e' +
+                   '	]' +
+                   '	/keyIndex 0' +
+                   '	/colorIndex 7' +
+                   '	/isOpen 1' +
+                   '	/eventCount 1' +
+                   '	/event-1 {' +
+                   '		/useRulersIn1stQuadrant 1' +
+                   '		/internalName (ai_plugin_pathfinder)' +
+                   '		/localizedName [ 10' +
+                   '			5061746866696e646572' +
+                   '		]' +
+                   '		/isOpen 1' +
+                   '		/isOn 1' +
+                   '		/hasDialog 0' +
+                   '		/parameterCount 1' +
+                   '		/parameter-1 {' +
+                   '			/key 1851878757' +
+                   '			/showInPalette 1' +
+                   '			/type (enumerated)' +
+                   '			/name [ 9' +
+                   '				496e74657273656374' +
+                   '			]' +
+                   '			/value 1' +
+                   '		}' +
+                   '	}' +
+                   '}',
+
+          f      = new File('~/ScriptAction.aia');
+
+      f.open('w');
+      f.write(actStr);
+      f.close();
+      app.loadAction(f);
+      f.remove();
+      app.doScript("Intersect_selection", "Intersect_selection", false); // action name, set name
+      app.unloadAction("Intersect_selection", ""); // set name
+    }
+  }
+
   /**
    * make knife-rectangles to trim text
    * @return {Array} rects - array of rectangls
@@ -29,7 +99,6 @@ function getMinLineWeight() {
     var rects = [];
 
     var amtRects = Math.ceil(_h / rectHeight);
-    $.writeln(amtRects);
 
     for (var i = 0, j = 0; i < amtRects; i++, j -= rectHeight) {
       var rect = rects[i];
@@ -41,58 +110,6 @@ function getMinLineWeight() {
     }
 
     return rects;
-  }
-
-  /**
-   * make action that try to intersect selection paths
-   * */
-  function _intersectSelection() {
-    var actStr = '' +
-                 '/version 3' +
-                 '/name [ 19' +
-                 '	496e746572736563745f73656c656374696f6e' +
-                 ']' +
-                 '/isOpen 0' +
-                 '/actionCount 1' +
-                 '/action-1 {' +
-                 '	/name [ 19' +
-                 '		496e746572736563745f73656c656374696f6e' +
-                 '	]' +
-                 '	/keyIndex 0' +
-                 '	/colorIndex 7' +
-                 '	/isOpen 1' +
-                 '	/eventCount 1' +
-                 '	/event-1 {' +
-                 '		/useRulersIn1stQuadrant 1' +
-                 '		/internalName (ai_plugin_pathfinder)' +
-                 '		/localizedName [ 10' +
-                 '			5061746866696e646572' +
-                 '		]' +
-                 '		/isOpen 1' +
-                 '		/isOn 1' +
-                 '		/hasDialog 0' +
-                 '		/parameterCount 1' +
-                 '		/parameter-1 {' +
-                 '			/key 1851878757' +
-                 '			/showInPalette 1' +
-                 '			/type (enumerated)' +
-                 '			/name [ 9' +
-                 '				496e74657273656374' +
-                 '			]' +
-                 '			/value 1' +
-                 '		}' +
-                 '	}' +
-                 '}',
-
-        f      = new File('~/ScriptAction.aia');
-
-    f.open('w');
-    f.write(actStr);
-    f.close();
-    app.loadAction(f);
-    f.remove();
-    app.doScript("Intersect_selection", "Intersect_selection", false); // action name, set name
-    app.unloadAction("Intersect_selection", ""); // set name
   }
 
   function _getMinWidth(selection) {
